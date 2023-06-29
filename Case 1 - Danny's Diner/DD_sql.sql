76df75af4218581e522922db25f35b7ca63a4308
Arvind Muralidharan <98005090+Arvindm22@users.noreply.github.com>
2023-06-29 16:09:11 +0530

    Added outputs

--- Case 1 - Danny's Diner/DD_sql.sql
+++ Case 1 - Danny's Diner/DD_sql.sql
@@ -5,15 +5,32 @@
 -- 1. What is the total amount each customer spent at the restaurant?
 SELECT 
 	customer_id,
-    sum(price) AS Total_Amount
+        sum(price) AS Total_Amount
 FROM sales 
 JOIN menu USING (product_id)
 GROUP BY customer_id
 ORDER BY Total_Amount;
 
+
+| customer_id | total_amount |
+| ----------- | ------------ |
+| C           | 36           |
+| B           | 74           |
+| A           | 76           |
+
+
 -- 2. How many days has each customer visited the restaurant?
 SELECT 
-		customer_id,
+	customer_id,
         count(DISTINCT order_date) as No_of_days_visited
 FROM sales
 GROUP BY customer_id;
+
+
+| customer_id | no_of_days_visited |
+| ----------- | ------------------ |
+| A           | 4                  |
+| B           | 6                  |
+| C           | 2                  |
+
+---
