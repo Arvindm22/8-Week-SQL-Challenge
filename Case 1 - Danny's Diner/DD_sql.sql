30dff7e95e2a5caa46731b54fb922fd615184600
Arvind Muralidharan <arvimurali.99@gmail.com>
2023-06-29 16:00:02 +0530

    Reuploaded the sql script

--- Case 1 - Danny's Diner/DD_sql.sql
+++ Case 1 - Danny's Diner/DD_sql.sql
@@ -10,3 +10,10 @@ FROM sales
 JOIN menu USING (product_id)
 GROUP BY customer_id
 ORDER BY Total_Amount;
+
+-- 2. How many days has each customer visited the restaurant?
+SELECT 
+		customer_id,
+        count(DISTINCT order_date) as No_of_days_visited
+FROM sales
+GROUP BY customer_id;
